fd85032794481be79c7e15598457b1adfb4f61ce
Christopher Vote <cbourjaily@gmail.com>
2025-05-12 04:05:37 -0700

    finished drafting the parse_loop

--- rpc/rpc_calculator.s
+++ rpc/rpc_calculator.s
@@ -65,20 +65,60 @@ check_for_args:
 	cmpq $1, %r14			# if true, only argc is in the register
 	jle exit_with_success		# jump to exit if above is true
 
-	movq $1, %r15			# Start at argv[1]
+	movq $1, %r15			# Sets %r15 to start at argv[1]
 
-parse_loop:	/* iterates through the strings and extracts values */
+parse_loop:	/* Iterates through the strings and extracts values */
 	cmpq %r15, %r14		# Checks if there are arguments left to parse
+	je validate_stack	# Exits loop if argc has been reached
 
+	/* Load argv[%r15] into %rdi for strtoll */
+	movq %r15, %rax
+	movq (%rbx, %rax, 8), %rdi		# Sets %rdi to equal argv[%r15]
 
+	/* Prepares endptr for strtoll */
+	subq $8, %rsp		# Allocate 8 bytes
+	movq %rsp, %rsi		# Sets rsi as the end pointer
 
+	/* Align stack and call strtoll to convert the string to long long */
+	movq %rsp, %r10
+	andq $-16, %rsp
+	call strtoll
+	movq %r10, %rsp		# Restore the stack
 
-parse_integer:
+	/* Check whether the entire string was converted. If so, the endpter is 0 */
+	movq (%rsi), %rcx	# %rcx is the endptr
+	cmpb $0, (%rcx)		# Checks the endptr against 0
+	je push_integer		# If yes, it is a valid integer
 
+	/* Moves the token to %rsi */
+	movq %rdi, %rsi		# %rsi now holds the token being compared
 
+       /* Check whether the token is the plus operator (if it is not an integer) */
+	lea op_plus(%rip), %rdi		# Loads the plus operator into %rdi
+	call strcmp		# Calls the C function to compare the token and the plus operator
+	test %rax, %rax		# Tests if %rax is equal
+	je do_addition		# If equal, the token matches the plus operator; jump to do_addition
 
-parse_operator:
+	/* Check whether the token is a minus operator (if it is not a plus operator) */
+        lea op_minus(%rip), %rdi         # Loads the minus operator into %rdi
+        call strcmp             # Calls the C function to compare the token and the minus operator
+        test %rax, %rax         # Tests if %rax is equal
+        je do_addition          # If equal, the token matches the plus operator; jump to do_addition
 
+        /* Check whether the token is a multiply operator (if it is not a plus operator) */
+        lea op_multiply(%rip), %rdi         # Loads the multiply operator into %rdi
+        call strcmp             # Calls the C function to compare the token and the multiply operator
+        test %rax, %rax         # Tests if %rax is equal
+        je do_addition          # If equal, the token matches the plus operator; jump to do_addition
+
+        /* Check whether the token is a divide operator (if it is not a plus operator) */
+        lea op_divide(%rip), %rdi         # Loads the divide operator into %rdi
+        call strcmp             # Calls the C function to compare the token and the divide operator
+        test %rax, %rax         # Tests if %rax is equal
+        je do_addition          # If equal, the token matches the plus operator; jump to do_addition
+
+	/* If the token is neither an integer nor a known operator, jump to parse_error */
+	jmp parse_error
 
 
 do_addition:
@@ -107,6 +147,15 @@ reduction_error_operands:
 
 
 reduction_error_operators:
+
+
+
+validate_stack:
+	/* Checks that there is exactly one value left on the operand stack */
+	movq %rsp, %rax
+	cmpq %r12, %rax		# Compares stack pointer to stack base
+	je print_result		# If equal, the stack is valid
+	jmp reduction_error_operators	# Otherwise, there are excessive values remaining
 
 
 
